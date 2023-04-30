f6067cea23e812650d6df25b55aed72d9d5a1650
HaniaKhater <hania.khater2@hotmail.com>
2023-04-30 08:09:21 +0200

    update README.md typo

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hania <hania@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 06:32:44 by hania             #+#    #+#             */
-/*   Updated: 2023/04/26 07:54:15 by hania            ###   ########.fr       */
+/*   Updated: 2023/04/30 08:06:23 by hania            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,36 @@
 int main()
 {
     Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    try {
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        // sp.addNumber(6);
+        // sp.addNumber(3);
+        // sp.addNumber(17);
+        // sp.addNumber(9);
+        // sp.addNumber(11);
+    } catch ( std::out_of_range &e ) {
+        std::cerr << e.what() << std::endl;
+        return 0;
+    }
+    std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+    std::cout << "Longest span: " << sp.longestSpan() << std::endl;
 
     Span multiple = Span(10000);
     std::vector<int> nums(10000);
-    std::srand(std::time(NULL));
-    std::generate(nums.begin(), nums.end(), std::rand);
+    std::srand( std::time(NULL) );
+    std::generate( nums.begin(), nums.end(), std::rand );
     try {
-        multiple.addNumbers(nums.begin(), nums.end());
+        multiple.addNumbers( nums.begin(), nums.end() );
     } catch ( std::out_of_range &e ) {
         std::cerr << e.what() << std::endl;
     }
     try {
-        std::cout << multiple.shortestSpan() << std::endl;
-        std::cout << multiple.longestSpan() << std::endl;
+        std::cout << "Shortest span: " << multiple.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << multiple.longestSpan() << std::endl;
     } catch ( std::logic_error &e ) {
         std::cerr << e.what() << std::endl;
     }
